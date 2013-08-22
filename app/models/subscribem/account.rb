7dffa0aaaf84f49b4ecae6a6183533b688db98a1
Steve <stevemargc@gmail.com>
2013-08-22 17:04:16 -0400

    set up subdomains so that users will have account to go to

--- app/models/subscribem/account.rb
+++ app/models/subscribem/account.rb
@@ -1,6 +1,6 @@
 module Subscribem
   class Account < ActiveRecord::Base
-    attr_accessible :name, :owner_attributes
+    attr_accessible :name, :subdomain, :owner_attributes
     belongs_to :owner, :class_name => "Subscribem::User"
     accepts_nested_attributes_for :owner
   end
--- spec/features/accounts/sign_up_spec.rb
+++ spec/features/accounts/sign_up_spec.rb
@@ -5,6 +5,7 @@ feature 'Accounts' do
     visit subscribem.root_url
     click_link 'Account Sign Up'
     fill_in 'Name', :with => 'Test'
+    fill_in 'Subdomain', :with => 'test'
     fill_in 'Email', :with => 'subscribem@example.com'
     fill_in 'Password', :with => 'password'
     fill_in 'Password confirmation', :with => 'password'
@@ -12,6 +13,6 @@ feature 'Accounts' do
     success_message = "Your account has been successfully created."
     page.should have_content(success_message)
     page.should have_content('Signed in as subscribem@example.com')
-    # page.current_url.should == "http://test.example.com/"
+    page.current_url.should == "http://test.example.com/subscribem/"
   end
 end
