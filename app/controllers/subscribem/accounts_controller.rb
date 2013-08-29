1a44997291aaee620ef7258275dc060ba3e65b87
Steve <stevemargc@gmail.com>
2013-08-29 13:53:14 -0400

    issue with subdomain uniqueness on page 23 geting www.example.com instead of example.com

--- app/controllers/subscribem/accounts_controller.rb
+++ app/controllers/subscribem/accounts_controller.rb
@@ -7,12 +7,15 @@ module Subscribem
       @account.build_owner
     end
     
-    def create
-      account = Account.create(params[:account])
-      env['warden'].set_user(account.owner.id, :scope => :user)
-      env['warden'].set_user(account.id, :scope => :account)
-      flash[:success] = "Your account has been successfully created."
-      redirect_to subscribem.root_url(:subdomain => account.subdomain)
+    def create      
+      @account = Subscribem::Account.new(params[:account])
+      if @account.save
+        flash[:success] = "Your account has been successfully created."
+        redirect_to subscribem.root_url(:subdomain => @account.subdomain)
+      else
+        flash[:error] = "Sorry, your account could not be created."
+        render :new
+      end
     end
   	
   end
--- app/models/subscribem/account.rb
+++ app/models/subscribem/account.rb
@@ -3,5 +3,6 @@ module Subscribem
     attr_accessible :name, :subdomain, :owner_attributes
     belongs_to :owner, :class_name => "Subscribem::User"
     accepts_nested_attributes_for :owner
+    validates :subdomain, :presence => true, :uniqueness => true
   end
 end
